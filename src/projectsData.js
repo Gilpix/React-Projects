28733f2ca516eb4c3981b69557c362230f4f3bf1
Gilpix <kulsinggh@gmail.com>
2021-02-16 08:38:54 -0500

    Home page data and assets added for Subscription List project

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,5 +1,5 @@
 export const projectsData = [
     { id: 1, name: "Birthday Reminder", link: "/birthday", learn: 'Components | UseState', image: "/assets/birthday.png" },
     { id: 2, name: "Github Users", link: "/github", learn: 'UseEffect', image: "/assets/github.png" },
-    { id: 3, name: "Subscription List", link: "/subscribe", learn: 'Multiple Input Forms', image: "/assets/github.png" }
+    { id: 3, name: "Subscription List", link: "/subscribe", learn: 'Multiple Input Forms', image: "/assets/subscribers.png" }
 ]; 
