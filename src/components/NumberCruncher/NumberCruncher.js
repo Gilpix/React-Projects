ed9e0a5c5c5f3cefd76670c38061d26eec567ec6
Gilpix <kulsinggh@gmail.com>
2021-03-14 09:44:03 -0400

    Added NumberCruncher component and file

---
+++ src/components/NumberCruncher/NumberCruncher.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../SharedComp/Header';
+
+
+function NumberCruncher() {
+
+    document.body.style = 'background: 	#4682B4';
+
+
+
+    return (
+        <>
+
+
+            <Header heading='Number Cruncher'></Header>
+
+
+        </>
+    )
+}
+
+export default NumberCruncher
