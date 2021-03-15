6fc466351e3ce7e7611bc65cc9f00f7457199458
Gilpix <kulsinggh@gmail.com>
2021-03-15 14:35:54 -0400

    href fixed for navbar links

--- src/components/SharedComp/Navbar.js
+++ src/components/SharedComp/Navbar.js
@@ -8,14 +8,14 @@ function Navbar() {
 
                 <nav id="navbar_top" className="navbar fixed-top navbar-expand-lg navbar-dark bg-primary " >
                     <div className="container">
-                        <a className="navbar-brand" href="#">ReactMania</a>
+                        <a className="navbar-brand" href="/">ReactMania</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="main_nav">
                             <ul className="navbar-nav ml-auto">
-                                <li className="nav-item"><a className="nav-link" href="#"> Home </a></li>
-                                <li className="nav-item"><a className="nav-link" href="#"> Basic </a></li>
+                                <li className="nav-item"><a className="nav-link" href="/#home"> Home </a></li>
+                                <li className="nav-item"><a className="nav-link" href="/#basicProjects"> Basic </a></li>
                             </ul>
                         </div>
                     </div>
