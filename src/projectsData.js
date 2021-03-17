5fd8e8a2e9562a7f48debdbfe9b8a8b509e02b44
Gilpix <kulsinggh@gmail.com>
2021-03-16 23:13:03 -0400

    NumberCruncher project details added in projectData JSON file for home page

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,5 +1,6 @@
 export const projectsData = [
     { id: 1, name: "Birthday Reminder", link: "/birthday", learn: 'Components | UseState', image: "/assets/birthday.png" },
     { id: 2, name: "Github Users", link: "/github", learn: 'UseEffect', image: "/assets/github.png" },
-    { id: 3, name: "Subscription List", link: "/subscribe", learn: 'Multiple Input Forms', image: "/assets/subscribers.png" }
+    { id: 3, name: "Subscription List", link: "/subscribe", learn: 'Multiple Input Forms', image: "/assets/subscribers.png" },
+    { id: 4, name: "Number Cruncher ", link: "/calci", learn: 'useReducer hook', image: "/assets/counter.png" }
 ]; 
