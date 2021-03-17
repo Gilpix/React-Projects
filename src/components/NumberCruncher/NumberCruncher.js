3e836d0e27cb4c0fafe37254e4f69d7167118493
Gilpix <kulsinggh@gmail.com>
2021-03-16 23:07:23 -0400

    useReducer added to maintain the counter state and Modal state

--- src/components/NumberCruncher/NumberCruncher.js
+++ src/components/NumberCruncher/NumberCruncher.js
@@ -3,15 +3,49 @@ import Header from '../SharedComp/Header';
 import Modal from './Modal'
 
 
+const initialState = {
+    count: 0,
+    isModalOpen: false
+}
+
+function reducer(state, action) {
+
+    switch (action.type) {
+        case 'INCREMENT':
+            return { ...state, count: state.count + 1, isModalOpen: false };
+        case 'DECREMENT':
+            return {
+                ...state, count: state.count - 1, isModalOpen: false
+            };
+        case 'NEGATIVE':
+            return { ...state, isModalOpen: true };
+        default:
+            throw new Error();
+    }
+}
 
 
 
 function NumberCruncher() {
-    document.body.style = 'background: 	#3a9da7';
+    document.body.style = 'background:  #3a9da7';
 
 
 
+    const [state, dispatch] = React.useReducer(reducer, initialState);
 
+    const clickHandle = (type) => {
+
+        if (state.count >= 1) {
+            if (type === 'INCREMENT')
+                dispatch({ type: 'INCREMENT' })
+            else if (type === 'DECREMENT')
+                dispatch({ type: 'DECREMENT' })
+        }
+        else if (state.count == 0 && type === 'INCREMENT')
+            dispatch({ type: 'INCREMENT' })
+        else
+            dispatch({ type: 'NEGATIVE' })
+    }
 
 
 
@@ -27,13 +61,13 @@ function NumberCruncher() {
 
                     </div>
                     <div className='col-6 col-md-4 plus my-auto order-2 order-sm-1'>
-                        <i className="fa fa-5x fa-minus-square cruncher-icon " ></i>
+                        <i className="fa fa-5x fa-minus-square cruncher-icon " onClick={() => { clickHandle('DECREMENT') }}></i>
                     </div>
                     <div className='col-12 col-md-4 my-auto order-1 order-sm-2'>
-                        <p className='counter-text m-0'>155</p>
+                        <p className='counter-text m-0'>{state.count}</p>
                     </div>
                     <div className='col-6 col-md-4 minus my-auto order-3 order-sm-3'>
-                        <i className="fa fa-5x fa-plus-square cruncher-icon " ></i>
+                        <i className="fa fa-5x fa-plus-square cruncher-icon " onClick={() => { clickHandle('INCREMENT') }}></i>
 
                     </div>
 
