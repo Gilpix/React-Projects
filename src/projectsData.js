cd4ef53f3f38c5fa5b36bb0b28126ea5d3116f42
Gilpix <kulsinggh@gmail.com>
2021-03-25 12:43:18 -0400

    updated images in projects data

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,6 +1,6 @@
 export const projectsData = [
-    { id: 1, name: "Birthday Reminder", link: "/birthday", learn: 'Components | UseState', image: "/assets/birthday.png" },
-    { id: 2, name: "Github Users", link: "/github", learn: 'UseEffect', image: "/assets/github.png" },
-    { id: 3, name: "Subscription List", link: "/subscribe", learn: 'Multiple Input Forms', image: "/assets/subscribers.png" },
-    { id: 4, name: "Number Cruncher ", link: "/calci", learn: 'useReducer hook', image: "/assets/counter.png" }
+    { id: 1, name: "Birthday Reminder", link: "/birthday", learn: 'Components | UseState', image: "/assets/9.png" },
+    { id: 2, name: "Github Users", link: "/github", learn: 'UseEffect', image: "/assets/10.png" },
+    { id: 3, name: "Subscription List", link: "/subscribe", learn: 'Multiple Input Forms', image: "/assets/11.png" },
+    { id: 4, name: "Number Cruncher ", link: "/calci", learn: 'useReducer hook', image: "/assets/12.png" }
 ]; 
