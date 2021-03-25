cd6f9bb62d62a6340babbea7074d4de5d41f3a99
Gilpix <kulsinggh@gmail.com>
2021-03-25 15:37:04 -0400

    customHook useFetch used inMoviesWorld component to get top 250 movies data from imdb api

--- src/components/MoviesWorld/MoviesWorld.js
+++ src/components/MoviesWorld/MoviesWorld.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Header from '../SharedComp/Header';
+
+import { useFetch } from "./useFetch";
+
+
+const url = 'https://imdb-api.com/en/API/Top250Movies/' + process.env.REACT_APP_SECRET_NAME;
+
 
 function MoviesWorld() {
+    document.body.style = 'background: rgba(104,151,187, 1)';
+
+    const { data } = useFetch(url);
+    // console.log(data)
+
     return (
         <div>
+            <Header heading='Movies World'></Header>
+
+
 
         </div>
     )
