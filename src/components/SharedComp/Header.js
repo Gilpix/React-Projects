0a937e472547b6a2273dc685941ecf0d65ee1e66
Gilpix <kulsinggh@gmail.com>
2021-03-28 17:01:25 -0400

    Header design and color updated

--- src/components/SharedComp/Header.js
+++ src/components/SharedComp/Header.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Header(props) {
     return (
-        <div>
-            <div className='mt-5'>
+        <div className='pt-3 pb-3' style={{ backgroundColor: "#fff", background: "linear-gradient(#4e54c8, #4e54c8)" }}>
+            <div className=''>
                 <h1 className='heading-text'  >{props.heading}</h1>
                 <hr className='heading-hr'></hr>
             </div>
             <div className='container mb-5 mt-3'>
                 <div className='w-100 float-left ' style={{ textAlign: 'left' }}>
-                    <a className="back-button " href="/"><span><i className="fa fa-chevron-left"></i></span><span className="back-text">back</span></a>
+                    <a className="back-button " href="/"><span><i className="fa fa-chevron-left transition"></i></span><span className="back-text">back</span></a>
                 </div>
                 <div className='w-50 float-right' style={{ textAlign: 'right' }}>
                     <a className="back-button " href="/">  </a>
