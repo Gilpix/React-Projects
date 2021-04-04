0492c30e750d53d3f5a0cc8649e4a6b16c8c5f0e
Gilpix <kulsinggh@gmail.com>
2021-04-04 12:19:30 -0400

    Create footer component

---
+++ src/components/SharedComp/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+
+function Footer() {
+    return (
+        <footer>
+
+
+        </footer>
+    )
+}
+
+export default Footer
