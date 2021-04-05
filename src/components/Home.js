9795e071f1d36bd51dea1f37bc447017a62d6122
Gilpix <kulsinggh@gmail.com>
2021-04-05 11:26:09 -0400

    feat - add new sction 'technologies' on home page with curved background

--- src/components/Home.js
+++ src/components/Home.js
@@ -182,6 +182,35 @@ function Home() {
 
 
 
+            {/* Show Technologies Used Section */}
+            <section id='curved-section' className='curved '>
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path fill="#fff" fill-opacity="1" d="M0,64L80,69.3C160,75,320,85,480,106.7C640,128,800,160,960,160C1120,160,1280,128,1360,112L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+                </svg>
+
+                <div className='curved-heading w-100'>
+                    <div className='container'>
+                        <div className='row  '>
+
+                            <div className='col-12 col-md-6 col-lg-4 col-xl-4 my-5'>
+                                <h1 className='home-heading-tex w-100'>Technologies</h1>
+                                <hr className='curved-heading-hr'></hr>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,64L80,69.3C160,75,320,85,480,106.7C640,128,800,160,960,160C1120,160,1280,128,1360,112L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+
+            </section>
+
+
+
+
+
+
         </main >
         <Footer></Footer>
     </>
