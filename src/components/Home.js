1d83d389bd2127eb99f881a3272f18cbb0485e43
Gilpix <kulsinggh@gmail.com>
2021-04-05 20:26:34 -0400

    other - import and integrate carousel for technologies section on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import Footer from "./SharedComp/Footer";
 
 import { projectsData } from ".././projectsData";
 
+import CarouselComponent from '../components/SharedComp/CarouselComponent';
 
 
 function Home() {
@@ -201,6 +202,7 @@ function Home() {
                     </div>
                 </div>
 
+                <CarouselComponent></CarouselComponent>
 
 
                 <svg className='pb-0 mb-0' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fillOpacity="1" d="M0,64L80,69.3C160,75,320,85,480,106.7C640,128,800,160,960,160C1120,160,1280,128,1360,112L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
